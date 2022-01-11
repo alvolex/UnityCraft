7a18bde1eca577fe5a2e4e3e492cabc785c486f9
avolkers <alexandervolkers@gmail.com>
2022-01-11 19:30:03 +0100

    18 Started working on the Jobs system that will be responsible for chunks

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,5 +1,10 @@
-using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
+using Unity.Rendering;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
@@ -19,6 +24,17 @@ public class Chunk : MonoBehaviour
         mr.material = atlas;
         blocks = new Block[width, height, depth];  // X, Y , Z
 
+        //Setup for unity Jobs / Burst compiler
+        var inputMeshes = new List<Mesh>(width * height * depth);
+        int vertexStart = 0;
+        int triangleStart = 0;
+        int meshCount = width * height * depth;
+        int m = 0; //Just a counter to see how many blocks we've created
+        var jobs = new ProcessMeshDataJob();
+
+        jobs.vertexStart = new NativeArray<int>(meshCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+        jobs.triangleStart = new NativeArray<int>(meshCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
+        
         //Populate our chunk with blocks and give them a position in world space. 
         for (int z = 0; z < depth; z++)
         {
@@ -27,6 +43,16 @@ public class Chunk : MonoBehaviour
                 for (int x = 0; x < width; x++)
                 {
                     blocks[x, y, z] = new Block(new Vector3(x, y, z), MeshUtils.BlockType.DIRT); //Here we set the block that will be on coordinate X Y Z to be a new block of type dirt in this case.
+                    inputMeshes.Add(blocks[x, y, z].mesh); //Add the mesh that we create in the line above into our mesh array
+                    var vertCount = blocks[x, y, z].mesh.vertexCount; //Get the amount of vertices in the current block
+                    var triCount = (int) blocks[x, y, z].mesh.GetIndexCount(0); //This will return the amount of triangles in the current mesh
+
+                    jobs.vertexStart[m] = vertexStart;
+                    jobs.triangleStart[m] = triangleStart;
+                    
+                    vertexStart += vertCount; //Add the amount of verts to our "index"
+                    triangleStart += triCount; //Add the amount of triangles to our "index"
+                    m++; //Update m since we now have added a new block.
                 }
             }
         }
