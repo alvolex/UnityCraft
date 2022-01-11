268378fbf5c814df63b43e78d7b48a4ba8a78e7c
avolkers <alexandervolkers@gmail.com>
2022-01-11 13:13:27 +0100

    7 Randomness to the top layers. Cubes not great.

--- Assets/WorldController.cs
+++ Assets/WorldController.cs
@@ -2,20 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class WorldController : MonoBehaviour
 {
     [SerializeField] private GameObject block;
     [SerializeField] private int worldSize = 2;
+    [SerializeField] private int width = 10;
+    [SerializeField] private int height = 2;
+    [SerializeField] private int depth = 10;
+    
 
     IEnumerator BuildWorld()
     {
-        for (int z = 0; z < worldSize; z++)
+        for (int z = 0; z < depth; z++)
         {
-            for (int y = 0; y < worldSize; y++)
+            for (int y = 0; y < height; y++)
             {
-                for (int x = 0; x < worldSize; x++)
+                for (int x = 0; x < width; x++)
                 {
+                    if (y >= height - 2 && Random.Range(0,100) > 50)
+                    {
+                        continue;
+                    }
+
                     Vector3 pos = new Vector3(x, y, z);
                     GameObject cube = Instantiate(block, pos, Quaternion.identity);
                     cube.name = $"{x} - {y} - {z}";
